dcca8f147177ade073909877532fc757052d1731
PncTSrc <pnctsrc@gmail.com>
2016-06-26 18:18:50 -0400

    Table is able to show some information; Changed some file names

--- pages/home/client/home.js
+++ pages/home/client/home.js
@@ -1,9 +1,145 @@
+Template.home.helpers ({
+	/*
+	settings_section: function(){
+		return {
+			fields:[
+				'name','code','requirements',
+				{key:'description', label:'Info', fn: function(key){
+					if (key.length>50){
+						return key.substring(0,50)+"...";
+					} else {
+						return key;
+					}
+				}},
+				{key:'course', label: 'Major', fn: function(key){
+					const course_obj = Course.find({id: key}).fetch()[0];
+					key = course_obj.subjects;
+					const ids = [];
+
+					for(var i = 0; i < key.length; i++){
+						const maj_obj = Subject.find({id: key[i].id}).fetch();
+						
+						if (maj_obj.length==0){
+							ids.push("unknown major");
+						} else {
+							const maj_name = maj_obj[0].name;
+							ids.push(maj_name);
+						};
+					};
+
+					if(ids.length == 0){
+						return "unknown";
+					} else {
+						return ids.toString();
+					};				
+				}},
+				{key:'course', label:'term', fn: function(key){
+					const course_obj = Course.find({id: key}).fetch()[0];
+					key = course_obj.term;
+					return Term.find({id: key}).fetch()[0].name;
+				}},
+			],
+		};
+	},*/
+
+	settings_course: function(){
+		return {
+			rowsPerPage: 5,
+			fields:[
+				{key:'name', label: 'Course'},
+				{key:'code', label:'Code'},
+				{key:'requirements', label:'Requirements'},
+				{key:'description', label:'Description', fn: function(key){
+					if (key.length>50){
+						return key.substring(0,50)+"...";
+					} else {
+						return key;
+					}
+				}},
+				{key:'subjects', label: 'Major', fn: function(key){
+					const ids = [];
+
+					for(var i = 0; i < key.length; i++){
+						const maj_obj = Subject.find({id: key[i].id}).fetch();
+						
+						if (maj_obj.length==0){
+							ids.push("unknown major");
+						} else {
+							const maj_name = maj_obj[0].name;
+							ids.push(maj_name);
+						};
+					};
+
+					if(ids.length == 0){
+						return "unknown";
+					} else {
+						return ids.toString();
+					};				
+				}},
+				{key:'term', label:'term', fn: function(key){
+					return Term.find({id: key}).fetch()[0].name;
+				}},
+				{key:'id', label:'Sections', fn: function(key){
+					const sec_obj = Section.find({course: key}).fetch();//this is an array
+					const sections = [];
+					for (var i = 0; i < sec_obj.length; i++) {
+						sections.push("Section: " + sec_obj[i].section);
+					};
+
+					if(sections.length==0){
+						return "No section";
+					} else {
+						return sections.toString();
+					}
+				}},
+				{key:'id', label:'Insructors', fn: function(key){
+					const sec_obj = Section.find({course: key}).fetch();//this is an array
+					const instructors = [];
+					for (var i = 0; i < sec_obj.length; i++) {
+						const instru_array = sec_obj[i].instructors;
+						for (var i = 0; i < instru_array.length; i++) {
+							const instru_id = instru_array[i];
+							const instru_obj = Instructor.find({id: instru_id}).fetch()[0];
+							var instru_name = instru_obj.first + " " + instru_obj.last;
+							if(instru_obj.first=="Staff" || instru_obj.last=="Staff") instru_name = "Staff";
+							instructors.push("Section: " + sec_obj[i].section + " - " + instru_name);
+						};
+					};
+
+					if(instructors.length==0){
+						return "No insructor"
+					} else {
+						return instructors.toString();
+					};
+				}},
+			],
+		};
+	},
+
+	courseData: function(){
+		return Course.find({},{skip:0,limit:5}).fetch();
+	},
+
+	sectionData: function(){
+		return Section.find({},{skip:0,limit:5}).fetch();
+	},
+
+	hasKeyword: function(){
+		//return !Keyword.find().fetch().length==0;
+		return true;
+	},
+})
+
 Template.searchForm.events ({
-  "submit form": function(events) {
-    events.preventDefault();
-    var keyword = events.target.keyword.value;
+  "submit form": function(event) {
+    event.preventDefault();
+    var keyword = event.target.keyword.value;
+    if(keyword==""){
+    	window.alert("Enter a keyword!");
+    	return;
+    }
+
     Meteor.call("keywordInsert", keyword);
-    events.target.keyword.value = "";
-    console.log("submitted!");
+    event.target.keyword.value = "";
   }
 });
--- server/startup.js
+++ server/startup.js
@@ -1,30 +1,26 @@
-import { Meteor } from 'meteor/meteor';
-
-Meteor.startup(() => {
-  // code to run on server at startup
-  // check to see if the collections are empty
-  // if so, read the classes.json file and
-  // create the new collections
+Meteor.startup(function(){
+  Keyword.remove({});
+  /*
+  Instructor.remove({});
+  Term.remove({});
+  Course.remove({});
+  Section.remove({});
+  Requirement.remove({});
+  Subject.remove({}); 
+  
   if (Instructor.find().count()>0) return;
   const fs = Npm.require('fs');
-  console.log("reading data");
   fs.readFile(
-  '/Users/tim/Desktop/cs152aj/deisAcademic/public/data/classes.json', 'utf8',
+  "D:\\Luyi's\\JBS2016\\deisAcademic\\public\\data\\classes.json", 'utf8',
     Meteor.bindEnvironment(function (err, data) {
         if (err) {
             console.log('Error: ' + err);
             return;
         }
-        console.log('parsing data');
-        console.log(data.length);
         data = JSON.parse(data);
-  //      console.log(data);
-        console.log('read data');
-        console.log(data.length);
         let i=0;
         for (i=0;i<data.length;i++){
           const d = data[i];
-          console.dir(d);
           if (d.type=="instructor") {
             Instructor.insert(d);
           }else if (d.type=="requirement"){
@@ -42,5 +38,5 @@ Meteor.startup(() => {
             console.log(d.type);
           }
         }
-    }));
-});
+    }));*/
+})
