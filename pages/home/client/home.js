c16182c4c1d414b7cf5d85ce9f1caa5f0f7081a5
PncTSrc <pnctsrc@gmail.com>
2016-07-17 01:25:16 -0400

    Optimized default result sorting
    #Now the priority of sorting is term>major code>course number

--- pages/home/client/home.js
+++ pages/home/client/home.js
@@ -54,39 +54,45 @@ Template.home.onRendered(function(){
 						homeDict.set('noResult', true);
 					} else {
 						const sorted_result = result.sort(function(a, b) {
-    						if(parseInt(a.term) < parseInt(b.term)){
-        						return 1;  
-    						}else if(parseInt(a.term) > parseInt(b.term)){
-        						return -1;
-    						}else{
-        						//for a
-        						let course_num_a = parseInt(a.code.match(/\d+/gi)[0]);
-								if(course_num_a < 10) course_num_a = "00" + course_num_a;
-								if(course_num_a >= 10 && course_num_a < 100) course_num_a = "0" + course_num_a;
-								const course_dep_a = a.code.substring(0, a.code.indexOf(" "));
-								const last_a = a.code.charAt(a.code.length - 1);
-								let comp_string_a;
-								if(/\w/i.test(last_a)){
-									comp_string_a = course_dep_a + course_num_a + last_a;
-								} else{
-									comp_string_a = course_dep_a + course_num_a + 0;
-								};
+    					//for a
+        				let course_num_a = parseInt(a.code.match(/\d+/gi)[0]);
+						if(course_num_a < 10) course_num_a = "00" + course_num_a;
+						if(course_num_a >= 10 && course_num_a < 100) course_num_a = "0" + course_num_a;
+						const course_dep_a = a.code.substring(0, a.code.indexOf(" "));
+						const last_a = a.code.charAt(a.code.length - 1);
+						let comp_string_a;
+						if(/\w/i.test(last_a)){
+							comp_string_a = course_num_a + last_a;
+						} else{
+							comp_string_a = course_num_a + "0";
+						};
 
-								//for b
-								let course_num_b = parseInt(b.code.match(/\d+/gi)[0]);
-								if(course_num_b < 10) course_num_b = "00" + course_num_b;
-								if(course_num_b >= 10 && course_num_b < 100) course_num_b = "0" + course_num_b;
-								const course_dep_b = b.code.substring(0, b.code.indexOf(" "));
-								const last_b = b.code.charAt(b.code.length - 1);
-								let comp_string_b;
-								if(/\w/i.test(last_b)){
-									comp_string_b = course_dep_b + course_num_b + last_b;
-								} else{
-									comp_string_b = course_dep_b + course_num_b + 0;
-								};
-								return comp_string_a.localeCompare(comp_string_b);
-    						}
-						});
+						//for b
+						let course_num_b = parseInt(b.code.match(/\d+/gi)[0]);
+						if(course_num_b < 10) course_num_b = "00" + course_num_b;
+						if(course_num_b >= 10 && course_num_b < 100) course_num_b = "0" + course_num_b;
+						const course_dep_b = b.code.substring(0, b.code.indexOf(" "));
+						const last_b = b.code.charAt(b.code.length - 1);
+						let comp_string_b;
+						if(/\w/i.test(last_b)){
+							comp_string_b = course_num_b + last_b;
+						} else{
+							comp_string_b = course_num_b + "0";
+						};
+
+    					if(parseInt(a.term) < parseInt(b.term)){
+        					return 1;  
+    					}else if(parseInt(a.term) > parseInt(b.term)){
+        					return -1;
+    					}else{
+        					const major_comp = course_dep_a.localeCompare(course_dep_b);
+        					if(major_comp != 0){
+        						return major_comp;
+        					} else {
+        						return comp_string_a.localeCompare(comp_string_b);
+        					}
+    					}
+					});
 						for(let i = 0; i < sorted_result.length; i++){
 							sorted_result[i].index = i;
 						};
@@ -155,37 +161,43 @@ Template.home.events ({
 					homeDict.set('noResult', true);
 				} else {
 					const sorted_result = result.sort(function(a, b) {
+    					//for a
+        				let course_num_a = parseInt(a.code.match(/\d+/gi)[0]);
+						if(course_num_a < 10) course_num_a = "00" + course_num_a;
+						if(course_num_a >= 10 && course_num_a < 100) course_num_a = "0" + course_num_a;
+						const course_dep_a = a.code.substring(0, a.code.indexOf(" "));
+						const last_a = a.code.charAt(a.code.length - 1);
+						let comp_string_a;
+						if(/\w/i.test(last_a)){
+							comp_string_a = course_num_a + last_a;
+						} else{
+							comp_string_a = course_num_a + "0";
+						};
+
+						//for b
+						let course_num_b = parseInt(b.code.match(/\d+/gi)[0]);
+						if(course_num_b < 10) course_num_b = "00" + course_num_b;
+						if(course_num_b >= 10 && course_num_b < 100) course_num_b = "0" + course_num_b;
+						const course_dep_b = b.code.substring(0, b.code.indexOf(" "));
+						const last_b = b.code.charAt(b.code.length - 1);
+						let comp_string_b;
+						if(/\w/i.test(last_b)){
+							comp_string_b = course_num_b + last_b;
+						} else{
+							comp_string_b = course_num_b + "0";
+						};
+
     					if(parseInt(a.term) < parseInt(b.term)){
         					return 1;  
     					}else if(parseInt(a.term) > parseInt(b.term)){
         					return -1;
     					}else{
-        					//for a
-        					let course_num_a = parseInt(a.code.match(/\d+/gi)[0]);
-							if(course_num_a < 10) course_num_a = "00" + course_num_a;
-							if(course_num_a >= 10 && course_num_a < 100) course_num_a = "0" + course_num_a;
-							const course_dep_a = a.code.substring(0, a.code.indexOf(" "));
-							const last_a = a.code.charAt(a.code.length - 1);
-							let comp_string_a;
-							if(/\w/i.test(last_a)){
-								comp_string_a = course_dep_a + course_num_a + last_a;
-							} else{
-								comp_string_a = course_dep_a + course_num_a + 0;
-							};
-
-							//for b
-							let course_num_b = parseInt(b.code.match(/\d+/gi)[0]);
-							if(course_num_b < 10) course_num_b = "00" + course_num_b;
-							if(course_num_b >= 10 && course_num_b < 100) course_num_b = "0" + course_num_b;
-							const course_dep_b = b.code.substring(0, b.code.indexOf(" "));
-							const last_b = b.code.charAt(b.code.length - 1);
-							let comp_string_b;
-							if(/\w/i.test(last_b)){
-								comp_string_b = course_dep_b + course_num_b + last_b;
-							} else{
-								comp_string_b = course_dep_b + course_num_b + 0;
-							};
-							return comp_string_a.localeCompare(comp_string_b);
+        					const major_comp = course_dep_a.localeCompare(course_dep_b);
+        					if(major_comp != 0){
+        						return major_comp;
+        					} else {
+        						return comp_string_a.localeCompare(comp_string_b);
+        					}
     					}
 					});
 					for(let i = 0; i < sorted_result.length; i++){
@@ -239,37 +251,43 @@ Template.home.events ({
 					homeDict.set('noResult', true);
 				} else {
 					const sorted_result = result.sort(function(a, b) {
+    					//for a
+        				let course_num_a = parseInt(a.code.match(/\d+/gi)[0]);
+						if(course_num_a < 10) course_num_a = "00" + course_num_a;
+						if(course_num_a >= 10 && course_num_a < 100) course_num_a = "0" + course_num_a;
+						const course_dep_a = a.code.substring(0, a.code.indexOf(" "));
+						const last_a = a.code.charAt(a.code.length - 1);
+						let comp_string_a;
+						if(/\w/i.test(last_a)){
+							comp_string_a = course_num_a + last_a;
+						} else{
+							comp_string_a = course_num_a + "0";
+						};
+
+						//for b
+						let course_num_b = parseInt(b.code.match(/\d+/gi)[0]);
+						if(course_num_b < 10) course_num_b = "00" + course_num_b;
+						if(course_num_b >= 10 && course_num_b < 100) course_num_b = "0" + course_num_b;
+						const course_dep_b = b.code.substring(0, b.code.indexOf(" "));
+						const last_b = b.code.charAt(b.code.length - 1);
+						let comp_string_b;
+						if(/\w/i.test(last_b)){
+							comp_string_b = course_num_b + last_b;
+						} else{
+							comp_string_b = course_num_b + "0";
+						};
+
     					if(parseInt(a.term) < parseInt(b.term)){
         					return 1;  
     					}else if(parseInt(a.term) > parseInt(b.term)){
         					return -1;
     					}else{
-        					//for a
-        					let course_num_a = parseInt(a.code.match(/\d+/gi)[0]);
-							if(course_num_a < 10) course_num_a = "00" + course_num_a;
-							if(course_num_a >= 10 && course_num_a < 100) course_num_a = "0" + course_num_a;
-							const course_dep_a = a.code.substring(0, a.code.indexOf(" "));
-							const last_a = a.code.charAt(a.code.length - 1);
-							let comp_string_a;
-							if(/\w/i.test(last_a)){
-								comp_string_a = course_dep_a + course_num_a + last_a;
-							} else{
-								comp_string_a = course_dep_a + course_num_a + 0;
-							};
-
-							//for b
-							let course_num_b = parseInt(b.code.match(/\d+/gi)[0]);
-							if(course_num_b < 10) course_num_b = "00" + course_num_b;
-							if(course_num_b >= 10 && course_num_b < 100) course_num_b = "0" + course_num_b;
-							const course_dep_b = b.code.substring(0, b.code.indexOf(" "));
-							const last_b = b.code.charAt(b.code.length - 1);
-							let comp_string_b;
-							if(/\w/i.test(last_b)){
-								comp_string_b = course_dep_b + course_num_b + last_b;
-							} else{
-								comp_string_b = course_dep_b + course_num_b + 0;
-							};
-							return comp_string_a.localeCompare(comp_string_b);
+        					const major_comp = course_dep_a.localeCompare(course_dep_b);
+        					if(major_comp != 0){
+        						return major_comp;
+        					} else {
+        						return comp_string_a.localeCompare(comp_string_b);
+        					}
     					}
 					});
 					for(let i = 0; i < sorted_result.length; i++){
