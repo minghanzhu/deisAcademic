78f466eaa7cec75ac882b87723fbe5258f24cd86
PncTSrc <pnctsrc@gmail.com>
2016-07-20 11:45:12 -0400

    Read courseList in calendar page

--- pages/calendarTest/client/cTest.js
+++ pages/calendarTest/client/cTest.js
@@ -2,11 +2,12 @@ import fullCalendar from 'fullcalendar';
 
 Template.calendarTest.onCreated(function(){
 	this.calendarDict = new ReactiveDict();
+	/*
 	this.calendarDict.set('courseList', [
 		"1163-001651", //COSI 11A, 1163
 		"1163-005186", //MATH 10a, 1163
 		"1163-005363", //MUS 101A, 1163
-	]);
+	]);*/
 })
 
 Template.calendarTest.onRendered(function(){
@@ -67,6 +68,71 @@ Template.calendarTest.helpers({
 		return Template.instance().calendarDict.get('courseList');
 	},
 
+	pullUserCourseList: function(event){
+		const dict = Template.instance().calendarDict;
+		if(dict.get("courseList")){
+			console.log("called!")
+			return;
+		};
+
+		Meteor.call("searchCourseWithP", "", "", [], "1400", "", {
+            days: [],
+            start: "",
+            end: ""
+        }, false, false,
+			function(err, result){
+				if(result.length != 0){
+					const sorted_result = result.sort(function(a, b) {
+    					//for a
+        				let course_num_a = parseInt(a.code.match(/\d+/gi)[0]);
+						if(course_num_a < 10) course_num_a = "00" + course_num_a;
+						if(course_num_a >= 10 && course_num_a < 100) course_num_a = "0" + course_num_a;
+						const course_dep_a = a.code.substring(0, a.code.indexOf(" "));
+						const last_a = a.code.charAt(a.code.length - 1);
+						let comp_string_a;
+						if(/\w/i.test(last_a)){
+							comp_string_a = course_num_a + last_a;
+						} else{
+							comp_string_a = course_num_a + "0";
+						};
+
+						//for b
+						let course_num_b = parseInt(b.code.match(/\d+/gi)[0]);
+						if(course_num_b < 10) course_num_b = "00" + course_num_b;
+						if(course_num_b >= 10 && course_num_b < 100) course_num_b = "0" + course_num_b;
+						const course_dep_b = b.code.substring(0, b.code.indexOf(" "));
+						const last_b = b.code.charAt(b.code.length - 1);
+						let comp_string_b;
+						if(/\w/i.test(last_b)){
+							comp_string_b = course_num_b + last_b;
+						} else{
+							comp_string_b = course_num_b + "0";
+						};
+
+        				const major_comp = course_dep_a.localeCompare(course_dep_b);
+        				if(major_comp != 0){
+        					return major_comp;
+        				} else {
+        					return comp_string_a.localeCompare(comp_string_b);
+        				}
+					});
+					let current_course = "";
+					for(let i = 0; i < sorted_result.length; i++){
+						if((sorted_result[i].code) === current_course){
+							current_course = sorted_result[i].code;
+							sorted_result.splice(i, 1);
+							i--;
+						};
+						current_course = sorted_result[i].code;					
+					}
+					for(let i = 0; i < sorted_result.length; i++){
+						sorted_result[i].index = i;
+					};
+					dict.set("courseList", sorted_result);
+				}
+		});
+	},
+
 	getContId: function(courseId){
 		return Course.findOne({id: courseId}).continuity_id;
 	},
@@ -77,10 +143,64 @@ Template.calendarTest.helpers({
 })
 
 Template.calendarTest.events({
+	/*
 	"click .js-create-event": function(event){
 		event.preventDefault();
-		const new_event = {
+		Meteor.call("searchCourseWithP", "", "", [], "1400", "", {
+            days: [],
+            start: "",
+            end: ""
+        }, false, false,
+			function(err, result){
+				if(result.length != 0){
+					const sorted_result = result.sort(function(a, b) {
+    					//for a
+        				let course_num_a = parseInt(a.code.match(/\d+/gi)[0]);
+						if(course_num_a < 10) course_num_a = "00" + course_num_a;
+						if(course_num_a >= 10 && course_num_a < 100) course_num_a = "0" + course_num_a;
+						const course_dep_a = a.code.substring(0, a.code.indexOf(" "));
+						const last_a = a.code.charAt(a.code.length - 1);
+						let comp_string_a;
+						if(/\w/i.test(last_a)){
+							comp_string_a = course_num_a + last_a;
+						} else{
+							comp_string_a = course_num_a + "0";
+						};
 
-		}
-	}
+						//for b
+						let course_num_b = parseInt(b.code.match(/\d+/gi)[0]);
+						if(course_num_b < 10) course_num_b = "00" + course_num_b;
+						if(course_num_b >= 10 && course_num_b < 100) course_num_b = "0" + course_num_b;
+						const course_dep_b = b.code.substring(0, b.code.indexOf(" "));
+						const last_b = b.code.charAt(b.code.length - 1);
+						let comp_string_b;
+						if(/\w/i.test(last_b)){
+							comp_string_b = course_num_b + last_b;
+						} else{
+							comp_string_b = course_num_b + "0";
+						};
+
+        				const major_comp = course_dep_a.localeCompare(course_dep_b);
+        				if(major_comp != 0){
+        					return major_comp;
+        				} else {
+        					return comp_string_a.localeCompare(comp_string_b);
+        				}
+					});
+					let current_course = "";
+					for(let i = 0; i < sorted_result.length; i++){
+						if((sorted_result[i].code) === current_course){
+							current_course = sorted_result[i].code;
+							sorted_result.splice(i, 1);
+							i--;
+						};
+						current_course = sorted_result[i].code;					
+					}
+					for(let i = 0; i < sorted_result.length; i++){
+						sorted_result[i].index = i;
+					};
+					console.log(sorted_result);
+				}
+		});
+	},*/
 })
--- server/methods.js
+++ server/methods.js
@@ -319,6 +319,261 @@ Meteor.methods ({
     return Course.find(searchQuery).fetch();
   },
 
+  searchCourseWithP: function(keyword, term, req_array, dept, prof, time, if_indept, if_not_sure){
+    keyword = keyword.replace(/ +/gi, " ");
+    keyword = keyword.trim();
+    const codes_record = [];//this records the user tokens
+    const keys_record = [];//this records all the matches
+    for(let item of codes){
+      //in the form of CODE + NUM + LETTER; for exmaple
+      //cosi11a, coSi 11a, COsi 400, COsI400
+      if(item.includes("/")){//some code is in the form of COSI/MATH
+        let indexOfSlash = item.indexOf("/");
+        let first_half = item.substring(0, indexOfSlash);
+        let second_half = item.substring(indexOfSlash + 1);
+        let regex_1 = new RegExp("( |^)" + first_half + " ?(\\d{1,3}[A-Z]{0,1})?( |$)", "i");
+        let regex_2 = new RegExp("( |^)" + second_half + " ?(\\d{1,3}[A-Z]{0,1})?( |$)", "i");
+
+        if(keyword.match(regex_1)){
+          let code_token = keyword.match(regex_1)[0];
+          let code_key = item + " " + code_token.trim().substring(first_half.length).trim().toUpperCase();
+          codes_record.push(code_token);
+          keys_record.push(code_key.trim());
+        } else if (keyword.match(regex_2)){
+          let code_token = keyword.match(regex_2)[0];
+          let code_key = item + " " + code_token.trim().substring(second_half.length).trim().toUpperCase();
+          codes_record.push(code_token);
+          keys_record.push(code_key.trim());
+        }
+      } else {
+        let regex = new RegExp("( |^)" + item + " ?(\\d{1,3}[A-Z]{0,1})?( |$)", "i");
+
+        if(keyword.match(regex)){
+          let code_token = keyword.match(regex)[0];
+          let code_key = item + " " + code_token.trim().substring(item.length).trim().toUpperCase();
+          codes_record.push(code_token);
+          keys_record.push(code_key.trim());
+        }
+      }
+    }
+
+    //this extracts the code token out of the keyword string
+    for(let key of codes_record){
+      if(keyword.match(key)){
+        keyword = keyword.replace(key, " ");
+        keyword = keyword.replace(/ {2, }/i, " ");
+      }
+    }
+
+    var regexCode;
+    if(keys_record.length != 0){
+      let new_keyword = "(" + keys_record[0];
+      for(let i = 1; i < keys_record.length; i++){
+        new_keyword = new_keyword + "|" + keys_record[i];
+      }
+      new_keyword = new_keyword + ")";
+      if(!if_not_sure){
+        if(!/\d/i.test(new_keyword)){
+          regexCode = new RegExp("^" + new_keyword + " \\d{1,3}([A-Z]{0,1})?$", "i");
+        } else{
+          regexCode = new RegExp("^" + new_keyword + " ?([A-Z]{0,1})?$", "i");
+        }
+      } else {
+        regexCode = new RegExp("^" + new_keyword + " ?((\\d{1,3})?[A-Z]{0,1})?$", "i");
+      };
+    } else {
+      regexCode = new RegExp("^", "i");
+    }
+
+    var regexTitle;
+    if(/^ +$/.test(keyword)){//this makes sure there's something left in the keyword string
+      regexTitle = new RegExp("^", "i");
+    } else {
+      regexTitle = new RegExp(keyword.trim(),"i");
+    };
+
+    var regexTerm = new RegExp("^" + term, "i");
+    let hasProfessor = false;
+    let searchQuery;
+    if(if_indept){
+      searchQuery = {term: regexTerm, code: regexCode, name: regexTitle};
+    } else {
+      searchQuery = {term: regexTerm, code: regexCode, name: regexTitle, independent_study: false};
+    }
+
+    //process the array of requirements
+    if(req_array.length != 0){
+      searchQuery.$and = [];
+      for(let node of req_array){
+        searchQuery.$and.push({requirements: node});
+      }
+    };
+
+    //term-dept
+    if(term && dept && dept !== "all"){//make term-dept
+      const dept_query = term + "-" + dept;
+      searchQuery['subjects.id'] = dept_query;
+    } else if (!term && dept && dept !== "all"){
+      let regexDept = new RegExp(dept + "$", "i");
+      searchQuery['subjects.id'] = regexDept;
+    }
+
+    //instructor
+    let prof_id;//the id for this professor
+    if(prof){
+      let section_of_prof;//array of all sections taught by the professor
+      for(let item of prof_name_and_id){
+        if(item.title === prof){
+          prof_id = item.id;
+          section_of_prof = Section.find({instructors: prof_id}).fetch();
+          hasProfessor = true;
+          break;
+        }
+      }
+
+      //if no professor matches, return no result
+      if(!section_of_prof){
+        return [];
+      } else if (section_of_prof.length == 0){
+        return [];
+      };
+
+      if(prof_id && section_of_prof){
+        let req_and = [];
+        if(req_array.length != 0){
+          req_and = searchQuery.$and;
+        }
+
+        const section_id_list = [];
+
+        for(let item of section_of_prof){
+          section_id_list.push(item.course);
+        }
+
+        const prof_or = {'$or':[]};
+
+        for(let item of section_id_list){
+          prof_or.$or.push({id:item})
+        }
+        searchQuery.$and = [prof_or];
+
+        if(req_and.length != 0){
+          for(let item of req_and){
+            searchQuery.$and.push(item);
+          }
+        }
+      }
+    }
+
+    //time and date
+    let days_array = time.days;
+    let search_start = time.start;
+    let search_end = time.end;
+    if(days_array.length != 0 || (search_start && search_start !== "all") || (search_end && search_end !== "all")){
+      const searchQuery_time = {$and:[]};
+      if(search_start && search_start !== "all"){
+        const start_hr = parseInt(search_start.substring(0, search_start.indexOf(":")));
+        const start_min = parseInt(search_start.substring(search_start.indexOf(":") + 1));
+        search_start = start_hr * 60 + start_min;
+      } else {
+        search_start = 0;
+      }
+
+      if(search_end && search_end !== "all"){
+        const end_hr = parseInt(search_end.substring(0, search_end.indexOf(":")));
+        const end_min = parseInt(search_end.substring(search_end.indexOf(":") + 1));
+        search_end = end_hr * 60 + end_min;
+      } else {
+        search_end = 1440;
+      }
+
+      if(search_start >= "0" && search_start <= "1440"){
+        searchQuery_time.$and.push({'times.start': {$gte: search_start, $lte: 1440}});
+      }
+
+      if(search_end >= "0" && search_end <= "1440"){
+        searchQuery_time.$and.push({'times.end': {$gte: 0, $lte: search_end}});
+      }
+
+      if(days_array.length != 0){
+        for(let day of days_array){
+          searchQuery_time.$and.push({'times.days': day});
+        }
+      }
+
+      const section_at_time = Section.find(searchQuery_time).fetch();
+      if(section_at_time.length != 0){
+        const section_id_list_time = [];
+        for(let item of section_at_time){
+         section_id_list_time.push(item.course);
+        }
+
+        if(searchQuery.$and){
+          if(!hasProfessor){
+            let time_id_or = {$or:[]};
+            for(let key of section_id_list_time){
+              time_id_or.$or.push({id:key})
+            }
+            searchQuery.$and.push(time_id_or);
+          } else {
+            //loop through the existring id list
+            for(let i = 0; i < searchQuery.$and.length; i++){
+              if (searchQuery.$and[i].$or){
+                if (searchQuery.$and[i].$or.length != 0){
+                  if (searchQuery.$and[i].$or[0].id){//make sure that the current node is an id search
+                    for(let j = 0; j < searchQuery.$and[i].$or.length; j++){
+                      //get the section using the current id
+                      let current_section;
+                      if(days_array.length != 0){
+                        let section_time_query = {
+                          course: searchQuery.$and[i].$or[j].id,
+                          instructors: prof_id,
+                          'times.start': {$gte: search_start, $lte: 1440},
+                          'times.end': {$gte: 0, $lte: search_end}
+                        };
+                        section_time_query.$and = [];
+                        for(let day of days_array){
+                          section_time_query.$and.push({'times.days': day});
+                        }
+                        current_section = Section.findOne(section_time_query);
+                      } else {
+                        current_section = Section.findOne({
+                          course: searchQuery.$and[i].$or[j].id,
+                          instructors: prof_id,
+                          'times.start': {$gte: search_start, $lte: 1440},
+                          'times.end': {$gte: 0, $lte: search_end}
+                        });
+                      };
+
+                      if(!current_section) {
+                        searchQuery.$and[i].$or.splice(j, 1);
+                        j--;
+                      }
+                    }
+                  }
+
+                  if(searchQuery.$and[i].$or.length == 0){
+                    return [];
+                  }
+                }
+              }
+            }
+          }
+        } else {
+          let time_id_or = {$or:[]};
+          for(let key of section_id_list_time){
+            time_id_or.$or.push({id:key})
+          }
+          searchQuery.$and = [time_id_or];
+        }
+      } else {
+        return [];
+      }
+    }
+
+    return Course.find(searchQuery,{fields:{_id:0,credits:0,requirements:0,subjects:0,type:0,term:0,independent_study:0}}).fetch();
+  },
+
   	searchTerm: function(key){
   		return Term.findOne({id: key}).name;
   	},
