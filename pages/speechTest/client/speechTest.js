bdaad9bf59d2de5c5955a5ab380f2f9b750b934b
Rob Moore <rmoore@brandeis.edu>
2016-07-21 00:54:42 -0400

    Cleaned up speechTest pages

--- pages/speechTest/client/speechTest.js
+++ pages/speechTest/client/speechTest.js
@@ -14,73 +14,21 @@ Template.speechTest.events({
 
     }
     recognition.start();
-    //
-    //
-    //     recognition = new webkitSpeechRecognition();
-    //
-    //     recognition.onstart = function(event) {
-    //       respond(messageRecording);
-    //       updateRec();
-    //     };
-    //     recognition.onresult = function(event) {
-    //       recognition.onend = null;
-    //
-    //       var text = "";
-    //       for (var i = event.resultIndex; i < event.results.length; ++i) {
-    //         text += event.results[i][0].transcript;
-    //       }
-    //       setInput(text);
-    //       stopRecognition();
-    //     };
-    //     recognition.onend = function() {
-    //       respond(messageCouldntHear);
-    //       recognition.stop();
-    //     };
-    //     recognition.lang = "en-US";
-    //     recognition.start();
+
   },
-  //
-  // "click .js-apiTest": function(){
-  //   HTTP.get("https://api.api.ai/v1/query?query=can%20i%20look%20at%20cosi%2011%20a&lang=en&confidence=0.6",
-  //   {headers:
-  //     {"Authorization": "Bearer ___________"}
-  //   },
-  //   function(error,result){
-  //     console.log(result)
-  //     console.log(result.data.result.parameters);
-  //     console.log("Intent" + result.data.result.metadata.intentName);
-  //   })
-  // },
 
-  "click .js-test2": function(){
+  "click .js-apiSubmit": function(){
 
-    // var test22;
-    //
     // $.get(".config.txt", function(data,status){
     //   console.log(data);
-    //   test22 = data;
-    //   console.log(test22);
-
     // });
-    // console.log(key);
 
     // const text = "can i see cosi 11 a";
+
     const text = Session.get("speechResults");
     console.log(text);
     const text2 = text.transcript;
     console.log(text);
-    // HTTP.post("https://api.api.ai/v1/query/",
-    //   {
-    //   contentType: "application/json; charset=utf-8",
-    //   dataType: "json",
-    //   headers: {
-    //     "Authorization": "Bearer __________"
-    //   },
-    // },
-    //   JSON.stringify({q: text, lang: "en"}),
-    //
-    // ),
-
 
 
     HTTP.call(
