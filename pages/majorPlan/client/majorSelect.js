b1a45ee58b61896450cdfb4ba51f6278fae5b114
PncTSrc <pnctsrc@gmail.com>
2016-07-29 11:00:29 -0400

    Fixed a bug of reading major list

--- pages/majorPlan/client/majorSelect.js
+++ pages/majorPlan/client/majorSelect.js
@@ -1,93 +1,98 @@
-Template.majorSelect.onCreated(function(){
-	this.majorSelectDict = new ReactiveDict();
-	this.majorSelectDict.set("clickedGo", false);
-	this.majorSelectDict.set("clickedHelp", false);
+Template.majorSelect.onCreated(function() {
+    this.majorSelectDict = new ReactiveDict();
+    this.majorSelectDict.set("clickedGo", false);
+    this.majorSelectDict.set("clickedHelp", false);
 })
 
-Template.majorSelect.onRendered(function(){
-	$('#search-select').dropdown();
-  	const major = $("#search-select input").val();
+Template.majorSelect.onRendered(function() {
+    $('#search-select').dropdown();
+    const major = $("#search-select input").val();
 
 });
 
 Template.majorSelect.helpers({
-	clickedGo: function(dict){
-		if(!$("#search-select input").val()){
-			window.alert("Please choose a major. \n Or click the button below.");
-			return;
-		};
-		dict.set("pageName", "chooseCourse");
-		dict.set("chosenMajor", $("#search-select input").val());
-	},
+    clickedGo: function(dict) {
+        if (!$("#search-select input").val()) {
+            window.alert("Please choose a major. \n Or click the button below.");
+            return;
+        };
+        dict.set("pageName", "chooseCourse");
+        dict.set("chosenMajor", $("#search-select input").val());
+    },
 
-	hasClickedGo: function(){
-		return Template.instance().majorSelectDict.get("clickedGo");
-	},
+    hasClickedGo: function() {
+        return Template.instance().majorSelectDict.get("clickedGo");
+    },
 
-	clickedHelp: function(dict){
-		
-		dict.set("pageName", "helpChooseMajor");
-		dict.set("chosenMajor", $("#search-select input").val());
-	},
+    clickedHelp: function(dict) {
 
-	hasClickedHelp: function(){
-		return Template.instance().majorSelectDict.get("clickedHelp");
-	},
+        dict.set("pageName", "helpChooseMajor");
+        dict.set("chosenMajor", $("#search-select input").val());
+    },
 
-	setMasterDict: function(dict){//this saves the master dict to the template
-		Template.instance().masterDict = dict;
-	},
+    hasClickedHelp: function() {
+        return Template.instance().majorSelectDict.get("clickedHelp");
+    },
+
+    setMasterDict: function(dict) { //this saves the master dict to the template
+        Template.instance().masterDict = dict;
+    },
 })
 
 Template.majorSelect.events({
-  	"click .js-majorGo": function(){
-    	event.preventDefault();
-    	if(!$("#search-select input").val()){
-			window.alert("Please choose a major. \nOr click the button below.");
-			return;
-		};
-		Template.instance().majorSelectDict.set("clickedGo", true);
-  	},
+    "click .js-majorGo": function() {
+        event.preventDefault();
+        if (!$("#search-select input").val()) {
+            window.alert("Please choose a major. \nOr click the button below.");
+            return;
+        };
+        Template.instance().majorSelectDict.set("clickedGo", true);
+    },
 
-  	"click .js-majorBulletin": function(){
-    	event.preventDefault();
-		Template.instance().majorSelectDict.set("clickedHelp", true);
-  	},
+    "click .js-majorBulletin": function() {
+        event.preventDefault();
+        Template.instance().majorSelectDict.set("clickedHelp", true);
+    },
 });
 
 
 Template.majorList.helpers({
-    scienceMajor: function() {
-      return Major.find({school: "School of Science"});
+    scienceMajorList: function() {
+        return Major.find({ school: "School of Science" });
     },
 
-	social: function() {
-		return Major.find({school: "School of Social Science"});
-	},
+    socialMajorList: function() {
+        return Major.find({ school: "School of Social Science" });
+    },
 
-	humanities: function() {
-		return Major.find({school: "School of Humanities"});
-	},
+    humanitiesMajorList: function() {
+        return Major.find({ school: "School of Humanities" });
+    },
 
-	major: function() {
-		return Major.findOne({name: homeDict.get('majorName')});
-	},	
+    creativeMajorList: function() {
+        return Major.find({ school: "School of Creative Arts" });
+    },
+
+    major: function() {
+        // return Major.find({name: homeDict.get('majorName')}).fetch()[0];
+        return Major.findOne({ name: homeDict.get('majorName') });
+    }
 });
 
 
 Template.majorList.events({
-	"click .bulletin-overlay,.js-close-popup": function(event){
-		$(".bulletin-overlay, .popup-bulletin").fadeToggle();
-	},
+    "click .bulletin-overlay,.js-close-popup": function(event) {
+        $(".bulletin-overlay, .popup-bulletin").fadeToggle();
+    },
 });
 
 Template.science.events({
-	"click .js-popup": function(event) {
-		const name = event.target.innerText;
-		console.log(name);
-		homeDict.set('majorName', name);
-		let popup = $(".popup-bulletin");
-		popup.css("top", (($(window).height() - popup.outerHeight()) / 2) + $(window).scrollTop() + 30 + "px");
-		$(".bulletin-overlay, .popup-bulletin").fadeToggle();
-	},
+    "click .js-popup": function(event) {
+        const name = event.target.innerText;
+        console.log(name);
+        homeDict.set('majorName', name);
+        let popup = $(".popup-bulletin");
+        popup.css("top", (($(window).height() - popup.outerHeight()) / 2) + $(window).scrollTop() + 30 + "px");
+        $(".bulletin-overlay, .popup-bulletin").fadeToggle();
+    },
 });
