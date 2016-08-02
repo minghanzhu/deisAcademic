01bf8f058fd2b2d3809b7623e56f56db827f2d22
PncTSrc <pnctsrc@gmail.com>
2016-08-02 01:37:57 -0400

    Skeleton profile page added

--- pages/myProfile/client/myProfile.js
+++ pages/myProfile/client/myProfile.js
@@ -1,63 +1,28 @@
-Template.myProfile.onCreated(function(){
-	this.myPageDict = new ReactiveDict();
-	this.myPageDict.set('pageName', "");
-	console.log(Template.instance().myPageDict.get('pageName'));
+Template.myProfile.onCreated(function() {
+    this.myPageDict = new ReactiveDict();
+    this.myPageDict.set('pageName', "");
 })
 
 
 Template.myProfile.helpers({
-  seeWishlist: function(){
-		return Template.instance().myPageDict.get('pageName')=="wishlist";
-	},
+    seeMajorPlan: function() {
+        return Template.instance().myPageDict.get('pageName') == "majorPlan";
+    },
 
-  seeHistory: function(){
-    return Template.instance().myPageDict.get('pageName')=="history";
-  },
+    seeSchedule: function() {
+        return Template.instance().myPageDict.get('pageName') == "schedule";
+    },
 })
 
 
 Template.myProfile.events({
-  "click .js-wishlist": function(event, template){
-    event.preventDefault();
-		console.log(Template.instance().myPageDict.get('pageName'));
-    template.myPageDict.set('pageName', "wishlist");
-  },
+    "click .js-majorPlan": function(event) {
+        event.preventDefault();
+        Template.instance().myPageDict.set('pageName', "majorPlan");
+    },
 
-  "click .js-history": function(event, template){
-    event.preventDefault();
-		console.log(Template.instance().myPageDict.get('pageName'));
-    template.myPageDict.set('pageName', "history");
-  },
-})
-
-
-
-
-
-Template.wishlist.helpers({
-	wishlist:function(){return Wishlist.find({});},
-})
-
-
-Template.history.onRendered(function(){
-	$('#search-select-semester').dropdown();
-	$('#search-select-coursenum').dropdown();
-  $('#search-select-grade').dropdown();
-  const coursenum = $("#search-select-coursenum input").val();
-
-})
-
-Template.history.helpers({
-  	coursehistory:function(){return Coursehistory.find({});},
-})
-
-Template.history.events({
-  "click .js-add": function(){
-    event.preventDefault();
-    const objsemester = $(".js-semester").val();
-    const objcoursenum = $(".js-coursenum").val();
-    const objgrade = $(".js-grade").val();
-    const obj = {semester: objsemester, coursenum: objcoursenum, grade:objgrade,};
-    Coursehistory.insert(obj);
-  },
+    "click .js-schedule": function(event) {
+        event.preventDefault();
+        Template.instance().myPageDict.set('pageName', "schedule");
+    },
 })
