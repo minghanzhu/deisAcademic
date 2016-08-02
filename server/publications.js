d853da54ecf3267cc112e44b691274777564af3e
PncTSrc <pnctsrc@gmail.com>
2016-08-02 12:54:41 -0400

    Home pub/sub finished

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,22 @@
-Meteor.publish("userProfilePnc", function(){return UserProfilePnc.find({userId: this.userId})});
-Meteor.publish("schedulesPnc", function(){return SchedulesPnc.find({userId: this.userId})});
-Meteor.publish("majorPlansPnc", function(){return MajorPlansPnc.find({userId: this.userId})});
-Meteor.publish("major", function(){return Major.find()});
-Meteor.publish("term", function(){return Term.find()});
+Meteor.publish("userProfilePnc", function() {
+    return UserProfilePnc.find({ userId: this.userId }) });
+Meteor.publish("schedulesPnc", function() {
+    return SchedulesPnc.find({ userId: this.userId }) });
+Meteor.publish("majorPlansPnc", function() {
+    return MajorPlansPnc.find({ userId: this.userId }) });
+Meteor.publish("major", function() {
+    return Major.find() });
+Meteor.publish("term", function() {
+    return Term.find() });
+
+
+Meteor.publish("home_userProfile", function() {
+    return UserProfilePnc.find({ 
+    	userId: this.userId 
+    },{
+    	fields:{
+    		userId: 1,
+    		wishlist: 1
+    	}
+    }) 
+});
