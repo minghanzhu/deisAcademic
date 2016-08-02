70dccbd219e578f9fbe5f775e6ae05b17a887003
“Minghan <minghanzhu@brandeis.edu>
2016-08-02 16:53:35 -0400

    functional side navigation bar

---
+++ client/layout/layout.js
@@ -0,0 +1,43 @@
+Template.layout.onRendered(function() {
+  $('.js-login span')
+  .popup({
+    content: "Sign in with your Brandeis email",
+    position: 'right center',
+  })
+;
+
+});
+
+Template.layout.events({
+  "click .js-login": function(event){
+    console.log("clicked");
+ 		event.preventDefault();
+ 		Meteor.loginWithGoogle(function(err, result){
+			if(err){
+				if(err.toString() === "Error: Please sign-up with a Brandeis Google account. [400]"){
+					window.alert(err);
+					return;
+				}
+				return;
+			};
+		});
+ 	},
+
+  "click .js-log-out": function(event){
+    event.preventDefault();
+    Meteor.logout();
+    Router.go('/');
+  },
+});
+
+Template.layout.helpers({
+  createAccount: function(){
+    if(Meteor.userId()){
+      Meteor.call("addUserProfile_Google",(new Date()).toLocaleString());
+    };
+  },
+
+  existsProfile: function(){
+    return UserProfilePnc.findOne({userId: Meteor.userId()})
+  },
+});
