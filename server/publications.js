b81ea0471e96431727bf848a5f89b20738b84414
PncTSrc <pnctsrc@gmail.com>
2016-09-26 22:58:45 -0400

    Security update
    #Users now are not able to subscribe to all major plans/schedules/user
    profiles by using Meteor.subscribe() at client.

--- server/publications.js
+++ server/publications.js
@@ -1,13 +1,5 @@
-Meteor.publish("userProfilePnc", function() {
-    return UserProfilePnc.find({ userId: this.userId }) });
-Meteor.publish("schedulesPnc", function() {
-    return SchedulesPnc.find({ userId: this.userId }) });
-Meteor.publish("majorPlansPnc", function() {
-    return MajorPlansPnc.find({ userId: this.userId }) });
 Meteor.publish("major", function() {
     return Major.find() });
-Meteor.publish("term", function() {
-    return Term.find() });
 
 
 Meteor.publish("home_userProfile", function() {
